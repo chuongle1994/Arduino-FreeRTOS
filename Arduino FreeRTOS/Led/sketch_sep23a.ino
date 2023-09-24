0061b9932e30bffae98b7c6a96c34851b2395fd5
Chuong <46336426+chuongle1994@users.noreply.github.com>
2023-09-24 12:19:40 -0400

    Added my Arduino project

---
+++ Arduino FreeRTOS/Led/sketch_sep23a.ino
@@ -0,0 +1,16 @@
+//#include <Arduino_FreeRTOS.h>
+
+#define led_pin 13
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(led_pin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    digitalWrite(led_pin, HIGH); //turn the led on
+    delay(10000);
+
+    digitalWrite(led_pin, LOW); //turn the led off
+    delay(10000);
+}
